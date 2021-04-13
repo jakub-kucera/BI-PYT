503b364978ccd203eb93470a972332790ddac176
Jakub Kučera <kucerj56@fit.cvut.cz>
2021-04-13 11:17:19 +0200

    Reverted back before implementing multithreading

--- homework03/filtering/filtering.py
+++ homework03/filtering/filtering.py
@@ -1,33 +1,16 @@
 import numpy as np
+
 import filtering.helpers
-from filtering.helpers import *
-from threading import Thread
 
 PADDING_VALUE = 0
+
+from filtering.helpers import *
 
 
 # def get_convolution_element_value(r: int, c: int, d: int, in_img: np.array, kernel: np.array):
 #     return PADDING_PIXEL_VALUE
 
 # out_img[r, c, d] = get_convolution_element_value(r, c, d, in_img, kernel)
-
-def worker_rgb(padded_img: np.array, flipped_kernel: np.array, out_img: np.array, dim: int, padding_size: int,
-               kernel_sides: int, img_rows: int, img_columns: int):
-    for img_row in range(padding_size, img_rows + padding_size + 1):
-        for img_col in range(padding_size, img_columns + padding_size + 1):
-            arr_window = padded_img[(img_row - kernel_sides):(img_row + kernel_sides + 1),
-                         (img_col - kernel_sides):(img_col + kernel_sides + 1), dim]
-            # flipped_arr = np.flip(arr_window, axis=(0, 1))
-            pixel_value = np.sum(arr_window * flipped_kernel)
-            # pixel_value = np.sum(flipped_arr * kernel)
-
-            if pixel_value < 0:
-                pixel_value = 0
-            elif pixel_value > 255:
-                pixel_value = 255
-
-            out_img[img_row, img_col, dim] = pixel_value
-
 
 def apply_filter_rgb(in_img: np.array, flipped_kernel: np.array) -> np.array:
     padding_size = int(flipped_kernel.shape[0] / 2 + 1)
@@ -44,34 +27,22 @@ def apply_filter_rgb(in_img: np.array, flipped_kernel: np.array) -> np.array:
     img_rows = in_img.shape[0]
     img_columns = in_img.shape[1]
     dimensions = in_img.shape[2]
-    #
-    workers = []
+
     for dim in range(dimensions):
-        args_dim = (padded_img, flipped_kernel, out_img, dim, padding_size, kernel_sides, img_rows, img_columns)
-        workers += [Thread(target=worker_rgb, args=args_dim)]
+        for img_row in range(padding_size, img_rows + padding_size + 1):
+            for img_col in range(padding_size, img_columns + padding_size + 1):
+                arr_window = padded_img[(img_row - kernel_sides):(img_row + kernel_sides + 1),
+                             (img_col - kernel_sides):(img_col + kernel_sides + 1), dim]
+                # flipped_arr = np.flip(arr_window, axis=(0, 1))
+                pixel_value = np.sum(arr_window * flipped_kernel)
+                # pixel_value = np.sum(flipped_arr * kernel)
 
-    for w in workers:
-        w.start()
-        # w.join()
+                if pixel_value < 0:
+                    pixel_value = 0
+                elif pixel_value > 255:
+                    pixel_value = 255
 
-    for w in workers:
-        w.join()
-    #
-    # for dim in range(dimensions):
-    #     for img_row in range(padding_size, img_rows + padding_size + 1):
-    #         for img_col in range(padding_size, img_columns + padding_size + 1):
-    #             arr_window = padded_img[(img_row - kernel_sides):(img_row + kernel_sides + 1),
-    #                          (img_col - kernel_sides):(img_col + kernel_sides + 1), dim]
-    #             # flipped_arr = np.flip(arr_window, axis=(0, 1))
-    #             pixel_value = np.sum(arr_window * flipped_kernel)
-    #             # pixel_value = np.sum(flipped_arr * kernel)
-    #
-    #             if pixel_value < 0:
-    #                 pixel_value = 0
-    #             elif pixel_value > 255:
-    #                 pixel_value = 255
-    #
-    #             out_img[img_row, img_col, dim] = pixel_value
+                out_img[img_row, img_col, dim] = pixel_value
 
     # return out_img
     return out_img[padding_size:img_rows + padding_size, padding_size:img_columns + padding_size, ]
@@ -132,14 +103,15 @@ def apply_filter(in_img: np.array, kernel: np.array) -> np.array:
     flipped_kernel = kernel
     # flipped_kernel = np.flip(flipped_kernel, axis=(0, 1))
 
+
     if flipped_kernel.shape[0] % 2 == 0:
         flipped_kernel = np.pad(flipped_kernel, ((0, 1), (0, 1)), 'constant',
                                 constant_values=((PADDING_VALUE, PADDING_VALUE), (PADDING_VALUE, PADDING_VALUE)))
     else:
         flipped_kernel = np.flip(flipped_kernel, axis=(0, 1))
 
-    # print(kernel)
-    # print(flipped_kernel)
+    print(kernel)
+    print(flipped_kernel)
 
     # flipped_kernel = np.flip(flipped_kernel, axis=(0, 1))
     #
@@ -154,12 +126,3 @@ def apply_filter(in_img: np.array, kernel: np.array) -> np.array:
 
     if in_img.ndim == 3:
         return apply_filter_rgb(in_img, flipped_kernel)
-
-
-"""
-ahoj, poradil by mi nekdo jak pracovat s tema cross kernelama u 3. ukolu?
-Jelikoz je to 2x2, tak jsem se to pres ruznej padding snazil zvetsit na 3x3 abych mel ten
-"prostredni" prvek a mohl s tim pracovat jako s tema ostatnima kernelama, ale vsechny
-kombinace co jsem zkousel nefungujou, vetsinou dostanu missmatch prvku kolem 50%.
-(pro ostatni kernely mi to pri tech zakladnich testech funguje)
-"""
